9246f96de4b9060acefdb76ac65f4861cffff155
Claytone 445 <brukva90@gmail.com>
2022-06-05 14:23:11 +0300

    added favicon

--- src/components/MainConverter.vue
+++ src/components/MainConverter.vue
@@ -4,13 +4,9 @@
   <div class="converter">
     <h1>{{ $t('main.convert') }}</h1>
     <p>{{ $t('main.enterAmount') }}</p>
-
     <label for="rubQauntity"> {{ $t('main.usdToRub', {rub: 'rub'}) }} </label>
     <v-text-field name="rubQauntity" id="rubQauntity" type="number" v-model.number="rub"></v-text-field>
-
-
     <v-text-field name="usdQauntity" id="usdbQauntity" type="number" v-model.number="convert"></v-text-field>
-
     <locale-switcher/>
   </div>
   </v-container>
